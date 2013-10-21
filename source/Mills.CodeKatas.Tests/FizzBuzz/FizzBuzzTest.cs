d824fe64760135e0fefab0adee7385710ae1f435
David Mills <moc tod zillm ta evad>
2013-10-21 11:12:21 -0400

    Initial commit + First Kata: Fizzbuzz

---
+++ source/Mills.CodeKatas.Tests/FizzBuzz/FizzBuzzTest.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mills.CodeKatas.FizzBuzz;
+using NUnit.Framework;
+
+namespace Mills.CodeKatas.Tests.FizzBuzz
+{
+    [TestFixture]
+    public class FizzBuzzTest
+    {
+        [Test]
+        public void FizzBuzz_NonFizzBuzzers()
+        {
+            FizzBuzzer fizzbuzzer = new FizzBuzzer();
+            Assert.AreEqual("1", fizzbuzzer.FizzBuzz(1));
+            Assert.AreEqual("2", fizzbuzzer.FizzBuzz(2));
+            Assert.AreEqual("4", fizzbuzzer.FizzBuzz(4));
+            Assert.AreEqual("7", fizzbuzzer.FizzBuzz(7));
+            Assert.AreEqual("8", fizzbuzzer.FizzBuzz(8));
+            Assert.AreEqual("11", fizzbuzzer.FizzBuzz(11));
+            Assert.AreEqual("14", fizzbuzzer.FizzBuzz(14));
+            Assert.AreEqual("98", fizzbuzzer.FizzBuzz(98));
+            Assert.AreEqual("101", fizzbuzzer.FizzBuzz(101));
+        }
+
+        [Test]
+        public void FizzBuzz_FizzOn3()
+        {
+            FizzBuzzer fizzbuzzer = new FizzBuzzer();
+            Assert.AreEqual("fizz", fizzbuzzer.FizzBuzz(3));
+        }
+
+        [Test]
+        public void FizzBuzz_BuzzOn5()
+        {
+            FizzBuzzer fizzbuzzer = new FizzBuzzer();
+            Assert.AreEqual("buzz", fizzbuzzer.FizzBuzz(5));
+        }
+
+
+        [Test]
+        public void FizzBuzz_FizzBuzzOn15()
+        {
+            FizzBuzzer fizzbuzzer = new FizzBuzzer();
+            Assert.AreEqual("fizzbuzz", fizzbuzzer.FizzBuzz(15));
+        }
+    }
+}
---
+++ source/Mills.CodeKatas/FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mills.CodeKatas.FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        public string FizzBuzz(int number)
+        {
+            if (number % 15 == 0)
+            {
+                return "fizzbuzz";
+            }
+
+            if (number % 3 == 0)
+            {
+                return "fizz";
+            }
+
+            if (number % 5 == 0)
+            {
+                return "buzz";
+            }
+
+            return number.ToString();
+        }
+    }
+}
