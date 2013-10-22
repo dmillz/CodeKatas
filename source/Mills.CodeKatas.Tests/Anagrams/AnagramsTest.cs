1a15452faa89c7738da5383ca6a1399ebcb57367
David Mills <moc tod zillm ta evad>
2013-10-22 18:52:29 -0400

    New Kata: Anagrams

---
+++ source/Mills.CodeKatas.Tests/Anagrams/AnagramsTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mills.CodeKatas.Anagrams;
+using NUnit.Framework;
+
+namespace Mills.CodeKatas.Tests.Anagrams
+{
+    [TestFixture]
+    public class AnagramsTest
+    {
+        [Test]
+        public void AbAndBaAreAnagrams()
+        {
+            AnagramAnalyzer anagramAnalyzer = new AnagramAnalyzer();
+            Assert.IsTrue(anagramAnalyzer.AreAnagrams("ab", "ba"));
+        }
+
+        [Test]
+        public void AbAndAcAreNotAnagrams()
+        {
+            AnagramAnalyzer anagramAnalyzer = new AnagramAnalyzer();
+            Assert.IsFalse(anagramAnalyzer.AreAnagrams("ab", "ac"));
+        }
+
+        [Test]
+        public void CapitalizationIsIgnored()
+        {
+            AnagramAnalyzer anagramAnalyzer = new AnagramAnalyzer();
+            Assert.IsTrue(anagramAnalyzer.AreAnagrams("AB", "ba"));
+        }
+
+        [Test]
+        public void SpacesAreIgnored()
+        {
+            AnagramAnalyzer anagramAnalyzer = new AnagramAnalyzer();
+            Assert.IsTrue(anagramAnalyzer.AreAnagrams("a b", "ba"));
+        }
+
+        [Test]
+        public void AllWhitespaceIsIgnored()
+        {
+            AnagramAnalyzer anagramAnalyzer = new AnagramAnalyzer();
+            Assert.IsTrue(anagramAnalyzer.AreAnagrams("a    \n b", "ba"));
+        }
+
+        [Test]
+        public void NullInputIsNeverAnAnagram()
+        {
+            AnagramAnalyzer anagramAnalyzer = new AnagramAnalyzer();
+            Assert.IsFalse(anagramAnalyzer.AreAnagrams(null, null));
+            Assert.IsFalse(anagramAnalyzer.AreAnagrams(null, ""));
+            Assert.IsFalse(anagramAnalyzer.AreAnagrams("", null));
+        }
+
+        [Test]
+        public void EmptyStringsAreAnagrams()
+        {
+            AnagramAnalyzer anagramAnalyzer = new AnagramAnalyzer();
+            Assert.IsTrue(anagramAnalyzer.AreAnagrams("", ""));
+        }
+    }
+}
---
+++ source/Mills.CodeKatas/Anagrams/AnagramAnalyzer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Mills.CodeKatas.Anagrams
+{
+    public class AnagramAnalyzer
+    {
+        public bool AreAnagrams(string s1, string s2)
+        {
+            if (s1 == null || s2 == null)
+            {
+                return false;
+            }
+
+            IDictionary<char, int> counts1 = CountChars(s1);
+            IDictionary<char, int> counts2 = CountChars(s2);
+
+            return counts1.Keys.All(c => counts2.ContainsKey(c) && counts1[c] == counts2[c]);
+        }
+
+        private IDictionary<char, int> CountChars(string s)
+        {
+            s = SanitizeInput(s);
+
+            IDictionary<char, int> counts = new Dictionary<char, int>();
+            foreach (char c in s)
+            {
+                if (!counts.ContainsKey(c))
+                {
+                    counts[c] = 0;
+                }
+                counts[c]++;
+            }
+
+            return counts;
+        }
+
+        private static string SanitizeInput(string s)
+        {
+            // Lowercase
+            s = s.ToLower();
+
+            // Remove whitespace 
+            s = Regex.Replace(s, @"\s+", "");
+
+            return s;
+        }
+    }
+}
